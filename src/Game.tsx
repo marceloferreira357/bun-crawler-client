f89b9699607ba7e48e530336acc472744e8b86d2
Marcelo Ferreira <104363673+marceloferreira357@users.noreply.github.com>
2024-08-17 12:41:44 -0300

    feat: added Entity component

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import { useShallow } from "zustand/react/shallow";
 import DebugCard from "./components/DebugCard";
+import Entity from "./components/Entity";
 import Scene from "./components/Scene";
 import useAnimationFrame from "./hooks/useAnimationFrame";
 import useWebSocket from "./hooks/useWebSocket";
@@ -30,6 +31,49 @@ function Game() {
     <div style={{ position: "relative" }}>
       <Scene size={{ width: "100dvw", height: "100dvh" }} update={update}>
         <DebugCard position={{ x: 8, y: 8 }} />
+        {/* entities debug */}
+        <Entity
+          visible
+          size={{
+            width: 64,
+            height: 64,
+          }}
+          zIndex={1}
+          position={{
+            x: 150,
+            y: 150,
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "red",
+              borderRadius: 12,
+            }}
+          />
+        </Entity>
+        <Entity
+          visible
+          size={{
+            width: 64,
+            height: 64,
+          }}
+          zIndex={1}
+          position={{
+            x: 300,
+            y: 300,
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "yellow",
+              borderRadius: 12,
+            }}
+          />
+        </Entity>
       </Scene>
     </div>
   );
---
+++ src/components/Entity.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef } from "react";
+import { Size, Vector2 } from "../common/types";
+
+type EntityProps = {
+  visible: boolean;
+  position?: Vector2;
+  size: Size;
+  zIndex: number;
+  children: React.ReactNode;
+};
+
+const Entity = forwardRef<HTMLDivElement, EntityProps>(
+  ({ visible, position, size, zIndex, children }, ref) => {
+    return (
+      <div
+        ref={ref}
+        style={{
+          position: position ? "absolute" : "static",
+          display: visible ? "block" : "none",
+          top: position?.x,
+          left: position?.y,
+          width: size.width,
+          height: size.height,
+          zIndex,
+        }}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Entity.displayName = "Entity";
+
+export default Entity;
