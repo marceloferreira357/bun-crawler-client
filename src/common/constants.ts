2a8b2d3dc11f6f0d19a71ec24dcd461fe1bd7287
Marcelo Ferreira <104363673+marceloferreira357@users.noreply.github.com>
2024-09-05 16:23:39 -0300

    chore: added indentation to mapTileDefaultAttributes constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -150,177 +150,351 @@ export const mapTileDefaultAttributes: MapTileDefaultAttributes = {
     },
   },
   wall_goo_base: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_goo: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_hole_1: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_hole_2: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_banner_blue: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_banner_red: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_banner_green: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_banner_yellow: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_ladder: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_spikes: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   hole: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_stairs: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_1: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_2: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_3: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_4: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_5: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_6: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_7: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   floor_8: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_mid: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_top_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_top_mid: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_top_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   button_red_up: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   button_red_down: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   button_blue_up: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   button_blue_down: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   lever_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   lever_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   doors_frame_left: {
-    size: { width: 16, height: 32 },
+    size: {
+      width: 16,
+      height: 32,
+    },
   },
   doors_frame_right: {
-    size: { width: 16, height: 32 },
+    size: {
+      width: 16,
+      height: 32,
+    },
   },
   doors_frame_top: {
-    size: { width: 32, height: 16 },
+    size: {
+      width: 32,
+      height: 16,
+    },
   },
   doors_leaf_closed: {
-    size: { width: 32, height: 32 },
+    size: {
+      width: 32,
+      height: 32,
+    },
   },
   doors_leaf_open: {
-    size: { width: 32, height: 32 },
+    size: {
+      width: 32,
+      height: 32,
+    },
   },
   wall_edge_bottom_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_bottom_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_mid_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_top_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_top_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_mid_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_tshape_bottom_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_tshape_bottom_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_tshape_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_edge_tshape_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_front_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_front_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_mid_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_top_left: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_top_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_outer_mid_right: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_fountain_top_1: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_fountain_top_2: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
   wall_fountain_top_3: {
-    size: { width: 16, height: 16 },
+    size: {
+      width: 16,
+      height: 16,
+    },
   },
 };
